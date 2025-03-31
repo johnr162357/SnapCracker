7d282c9ae5938efd69dee5bb7d0d2b275189f392
johnr162357 <johnathanrange770@gmail.com>
2025-03-31 02:55:00 +0000

    Update SnapHack.sh

--- SnapHack.sh
+++ SnapHack.sh
@@ -47,7 +47,7 @@ printf "\e[1;91m                   SnapCracker (Telegram-Alt_Gr)  \e[0m\n"
 
 
 function start() {
-read -p $'\e[1;92mUsername account: \e[0m' user
+read -p $'\e[1;92mserenity244827 account: \e[0m' user
 checkaccount=$(curl -L -s https://www.snapchat.com/$user/ | grep -c "the page may have been removed")
 if [[ "$checkaccount" == 1 ]]; then
 printf "\e[1;91mInvalid Username! Try again\e[0m\n"
